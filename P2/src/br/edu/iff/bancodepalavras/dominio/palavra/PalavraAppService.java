633beca15d4b0de7330b4721e729e5f8d46e53f8
Arthur Zem de Souza <arthurzem1612@gmail.com>
2022-10-31 18:50:29 -0300

    Atualizacao

--- P2/src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
+++ P2/src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
@@ -2,4 +2,23 @@ package br.edu.iff.bancodepalavras.dominio.palavra;
 
 public class PalavraAppService {
 
+    private static PalavraAppService soleinstance;
+    
+    public static void createSoleInstance(TemaRepository temaRepository, PalavraRepository palavraRepository, PalavraFactory palavraFactory) {
+        
+    }
+    
+    public static PalavraAppService getSoleInstance() {
+        
+    }
+    
+    private PalavraAppService(TemaRepository temaRepository, PalavraRepository palavraRepository, PalavraFactory factory) {
+        
+    }
+    
+    public boolean novaPalavra(String palavra, long idTema) {
+        
+    }
+    
+    
 }
--- P2/src/br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
+++ P2/src/br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
@@ -1,5 +1,36 @@
 package br.edu.iff.jogoforca.dominio.rodada;
 
+
 public class RodadaAppService {
 
+    private static RodadaAppService soleInstance;
+    
+    public static void createSoleInstance(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
+
+    }
+    
+    public static RodadaAppService getSoleInstance() {
+        
+    }
+    
+    private RodadaAppService(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
+        
+    }
+    
+    public Rodada novaRodada(long idJogador) {
+        
+    }
+    
+    public Rodada novaRodada(String nomeJogador) throws JogadorNaoEncontradoException{
+        
+    }
+    
+    public Rodada novaRodada(Jogador jogador) {
+        
+    }
+    
+    public boolean salvarRodada(Rodada rodada) {
+        
+    }
+    
 }
